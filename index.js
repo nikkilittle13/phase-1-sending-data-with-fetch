4bb8aa4a2b1729b079556d5bd2853e8c0c7c7551
Nikki Little <nikkilittle13@gmail.com>
2024-01-14 19:53:45 -0500

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your code here
+function submitData(name, email) {
+  let userData = {
+    name: name,
+    email: email
+  };
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(userData)
+  })
+  .then(response => response.json())
+  .then(data => {
+    document.body.innerHTML = data.id;
+  })
+  .catch(error => {
+    document.body.innerHTML = error.message;
+  });
+}
